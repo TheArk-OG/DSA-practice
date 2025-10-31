30697e12fda967b5be401f298361ace77effb865
Arman Khan <ak9934816676@gmail.com>
2025-10-31 21:41:01 +0530

    Day5_Top_k_Frequent_Element

---
+++ Day5_Top_k_Frequent_Element.cpp
@@ -0,0 +1,24 @@
+// Top_k_Frequent_Element
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        vector<pair<int, int>> arr;
+        for (const auto& p : count) {
+            arr.push_back({p.second, p.first});
+        }
+        sort(arr.rbegin(), arr.rend());
+
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(arr[i].second);
+        }
+        return res;
+    }
+};
